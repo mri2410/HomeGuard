392b85447d5e97a072759d60afd9b587a6983be8
mri2410 <mri2410@vt.edu>
2014-11-27 01:05:47 +0000

    2nd commit: Addign exceptions

--- send_sms_twilio.py
+++ send_sms_twilio.py
@@ -12,23 +12,28 @@ def sendTwilioSMS(sms_body):
 	# Account Sid and Auth Token from twilio.com/user/account
 	account_sid = "AC76d77770817e9ad7884f16430d23fbdf"	# Account ID
 	auth_token  = "7575b18d79cee2797c1ccf35360cb09c"	# Authentication Token
-	client = TwilioRestClient(account_sid, auth_token)	# Creating a client
- 
-	print sms_body
-
-	# If the url link is empty, then it shouldn't send a sms
-	if not sms_body:
-		print "Error: No content found to send the SMS."
-		print "Visitor needs to type a message to send it to the host.\n"
-		#sys.exit()
 	
-	else:
-		sms_body = "Visitor spotted: " + sms_body
+	try:
+		client = TwilioRestClient(account_sid, auth_token)	# Creating a client
+ 
 		print sms_body
-		message = client.sms.messages.create(body=sms_body,\
+
+		# If the url link is empty, then it shouldn't send a sms
+		if not sms_body:
+			print "Error: No content found to send the SMS."
+			print "Visitor needs to type a message to send it to the host.\n"
+			#sys.exit()
+	
+		else:
+			sms_body = "Visitor spotted: " + sms_body
+			print sms_body
+			message = client.sms.messages.create(body=sms_body,\
 			# Cell number, Twilio Number
 			to = "+15712652653", from_= "+15054046300")
-		print message.sid
+			print message.sid
+
+	except twilio.TwilioRestException as error:
+		print error
 
 sendTwilioSMS('Testing message.')
 #lease?! I love you <3",
